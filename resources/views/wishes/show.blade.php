c876e0b04456cdb867afd08c7f552c8193231fda
Ivan Kulikov <ikoolik@gmail.com>
2016-05-28 14:28:46 +0300

    обновлена страница просмотра

--- resources/views/wishes/show.blade.php
+++ resources/views/wishes/show.blade.php
@@ -4,25 +4,23 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            <h1>{{ $wish->name }}</h1>
             <div class="row">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img src="{{ $wish->image_url }}" class="img-thumbnail">
+                <div class="col-md-3">
+                    <img src="{{ $wish->image_url }}" class="img-thumbnail">
+                </div>
+                <div class="col-md-7">
+                    <div>
+                        {!! $wish->description !!}
                     </div>
-                    <div class="col-md-7">
-                        <h1>{{ $wish->name }}</h1>
-                        <div>
-                            {!! $wish->description !!}
-                        </div>
 
-                        <hr>
-                        <a class="btn btn-default" href="{{ route('wishes.edit', $wish->id) }}"><i class="fa fa-pencil"></i> Изменить</a>
-                    </div>
-                    <div class="col-md-2">
-                        {{ Form::open(['method' => 'DELETE', 'route' => ['wishes.destroy', $wish->id], 'class' => 'form-inline']) }}
-                        {{ Form::button('<i class="fa fa-trash"></i> Удалить', ['type' => 'submit', 'class' => 'btn btn-danger']) }}
-                        {{ Form::close() }}
-                    </div>
+                    <hr>
+                    <a class="btn btn-default" href="{{ route('wishes.edit', $wish->id) }}"><i class="fa fa-pencil"></i> Изменить</a>
+                </div>
+                <div class="col-md-2">
+                    {{ Form::open(['method' => 'DELETE', 'route' => ['wishes.destroy', $wish->id], 'class' => 'form-inline']) }}
+                    {{ Form::button('<i class="fa fa-trash"></i> Удалить', ['type' => 'submit', 'class' => 'btn btn-danger']) }}
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
