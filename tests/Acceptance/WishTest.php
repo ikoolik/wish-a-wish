4e0946dcfbf204e613ce88ef4986cc8ad3ca9ce2
Ivan Kulikov <ikoolik@gmail.com>
2016-05-29 07:24:45 +0300

    more tests

--- tests/Acceptance/WishTest.php
+++ tests/Acceptance/WishTest.php
@@ -13,74 +13,88 @@ class WishTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function it_shows_the_add_wish_link()
-    {
-        Auth::login(factory(User::class)->create());
-
-        $this->visit(route('wishes.index'))->seeStatusCode(200)->see('Новое желание');
-    }
-
-    /** @test */
-    public function it_shows_the_wish_in_the_list()
+    public function it_shows_wish_to_owner()
     {
         $user = factory(User::class)->create();
         $wish = factory(Wish::class)->create(['user_id' => $user->id]);
         Auth::login($user);
 
-        $this->visit(route('wishes.index'))->seeStatusCode(200)->see($wish->name);
+        $this->visit(route('wishes.show', $wish->id))
+            ->seeStatusCode(200)
+            ->see($wish->name)
+            ->see($wish->description)
+            ->see('Изменить')
+            ->see('Удалить');
     }
 
     /** @test */
-    public function it_shows_someones_list_to_unauthorized_user()
-    {
-        $someone = factory(User::class)->create();
-
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200);
-    }
-
-    /** @test */
-    public function it_shows_someones_wishes_to_unauthorized_user()
+    public function it_shows_someones_wish_to_unauthorized_user()
     {
         $someone = factory(User::class)->create();
         $wish = factory(Wish::class)->create(['user_id' => $someone->id]);
 
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200)->see($wish->name);
+        $this->visit(route('wishes.show', $wish->id))
+            ->seeStatusCode(200)
+            ->see($wish->name)
+            ->see($wish->description)
+            ->dontSee('Изменить')
+            ->dontSee('Удалить');
     }
 
     /** @test */
-    public function it_does_not_show_add_wish_link_in_someones_list_to_unauthorized_user()
+    public function it_shows_someones_wish_to_authorized_user()
     {
         $someone = factory(User::class)->create();
-
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200)->dontSee('Новое желание');
-    }
-
-    /** @test */
-    public function it_shows_someones_list_to_authorized_user()
-    {
-        $someone = factory(User::class)->create();
+        $wish = factory(Wish::class)->create(['user_id' => $someone->id]);
         Auth::login(factory(User::class)->create());
 
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200);
+        $this->visit(route('wishes.show', $wish->id))
+            ->seeStatusCode(200)
+            ->see($wish->name)
+            ->see($wish->description)
+            ->dontSeeLink('Изменить')
+            ->dontSee('Удалить');
     }
 
     /** @test */
-    public function it_shows_someones_wishes_to_authorized_user()
+    public function it_shows_edit_form_to_owner()
+    {
+        $user = factory(User::class)->create();
+        $wish = factory(Wish::class)->create(['user_id' => $user->id]);
+        Auth::login($user);
+
+        $this->visit(route('wishes.edit', $wish->id))
+            ->seeStatusCode(200)
+            ->see($wish->name)
+            ->see($wish->description)
+            ->see('Сохранить');
+    }
+
+    /** @test */
+    public function it_does_not_show_edit_form_to_unauthorized_user()
+    {
+        $someone = factory(User::class)->create();
+        $wish = factory(Wish::class)->create(['user_id' => $someone->id]);
+
+        $this->visit(route('wishes.edit', $wish->id))
+            ->see($wish->name)
+            ->see($wish->description)
+            ->seePageIs(route('wishes.show', $wish->id))
+            ->see('Ошибка!');
+    }
+
+    /** @test */
+    public function it_does_not_show_edit_form_to_authorized_user()
     {
         $someone = factory(User::class)->create();
         $wish = factory(Wish::class)->create(['user_id' => $someone->id]);
 
         Auth::login(factory(User::class)->create());
 
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200)->see($wish->name);
-    }
-
-    /** @test */
-    public function it_does_not_show_add_wish_link_in_someones_list_to_authorized_user()
-    {
-        $someone = factory(User::class)->create();
-        Auth::login(factory(User::class)->create());
-
-        $this->visit(route('wishes.user_index', $someone->id))->seeStatusCode(200)->dontSee('Новое желание');
+        $this->visit(route('wishes.edit', $wish->id))
+            ->see($wish->name)
+            ->see($wish->description)
+            ->seePageIs(route('wishes.show', $wish->id))
+            ->see('Ошибка!');
     }
 }
