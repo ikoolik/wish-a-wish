634d14eba962a7b91065667d2fc92f1bb93ae899
Ivan Kulikov <ikoolik@gmail.com>
2016-06-23 23:25:55 +0300

    обновил внешний вид панелек

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+            <div class="panel panel-default material">
                 <div class="panel-heading">Добро пожаловать!</div>
 
                 <div class="panel-body">
