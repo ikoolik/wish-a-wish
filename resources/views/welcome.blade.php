5cf4047a810da02dd0ca168de250e435535e29bf
Ivan Kulikov <ikoolik@gmail.com>
2016-06-24 21:19:06 +0300

    ссылки на GH

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
                 <div class="panel-body">
                     <p>Wish a Wish - это миниатюрный wishlist сервис, в котором нет ничего лишнего. Глядя правде в глаза, хочется отметить, что тут и нужного много чего нет, но проект будет развиваться и потихоньку обрастать мясом.<p>
                     <p>Все ваши вопросы и предложения вы можете отправлять на почту <a href="mailto:ivan@wish-a-wish.ru">ivan@wish-a-wish.ru</a></p>
+                    <p>Если же вы обладаете навыками разработки - не стесняйтесь оформить pull-request в <a href="https://github.com/ikoolik/wish-a-wish">ikoolik/wish-a-wish</a></p>
                 </div>
             </div>
         </div>
