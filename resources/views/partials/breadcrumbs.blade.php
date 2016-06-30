3379c09ebc7e783444e660a40e67f692cf63d648
Ivan Kulikov <ikoolik@gmail.com>
2016-06-30 20:16:18 +0300

    hide breadcrumbs separator on small screens

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -3,7 +3,7 @@
         @foreach ($breadcrumbs as $breadcrumb)
             @if (!$breadcrumb->last)
                 <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-                <li class="separator">|</li>
+                <li class="separator hidden-xs">|</li>
             @else
                 <li class="active">{{ $breadcrumb->title }}</li>
             @endif
