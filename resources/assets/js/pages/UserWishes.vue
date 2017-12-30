acb96c690c5639a710fdacc1920497cc0c0f72fe
Ivan Kulikov <ikoolik@gmail.com>
2017-12-30 11:18:56 +0300

    add header to user wishes page

--- resources/assets/js/pages/UserWishes.vue
+++ resources/assets/js/pages/UserWishes.vue
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1" v-if="user">
+                <h1>{{ user.name }}</h1>
                 <div class="row">
                     <div v-if="isMe" class="col-sm-4 col-md-3">
                         <new-wish></new-wish>
